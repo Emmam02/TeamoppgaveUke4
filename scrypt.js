79c7019c06f7f74edc91c25018aa14e4dec37aa1
Emmam02 <135839715+Emmam02@users.noreply.github.com>
2023-08-30 12:18:22 +0200

    DD
    DDDD

--- scrypt.js
+++ scrypt.js
@@ -8,23 +8,29 @@ var thirst = 0;
 
 var clean = true;
 
-var petAffection = 100;
+var petAffection = 10;
+
 /* Veiw */
 updateView();
 
 
 function updateView(){
-    console.log('updateView started');
+    console.log('اupdateView started');
 
 
-    document.getElementById('app').innerHTML= /*HTML*/ `
-    <div id="status"><strong id="title">Status Bar: ${tamaCoinBalance} hunger: ${hunger} thirst: ${thirst}</strong></div>
+    
+    document.getElementById('app').innerHTML = `<img class="my_tama"src="https://media.tenor.com/Br7nM85ym6oAAAAC/tamagotchi-bandai.gif" alt"TAMA_ERR">`;
+    
+    document.getElementById('app').innerHTML+= /*HTML*/ `
+    <div id="status"><strong id="title">Status Bar: TMC balance ${tamaCoinBalance} Hunger: ${hunger} Thirst: ${thirst} Affection: ${petAffection}</strong></div>
     <div id="Pet"></div>
     <button onclick="wash()">Wash your pet</button>
     <button onclick="feed()">Give Food</button>
     <button onclick="drink()">Give water</button>
     <button onclick="pet()">Pet your pet</button>
-    `;
+    <button onclick="Play()">Play with pet</button>
+    <button onclick="Sleep()">Sleep</button>
+    <button onclick="buyTamaCoin()">Buy Tama Coins</button>`;
     console.log('updateView finished');
 } 
 /* Controls */
@@ -36,8 +42,13 @@ function pet(){ // for å banke til dyret
 }
 
 function feed(){ // Mating av dyret
-    hunger += 10;
-    petAffection += 1;
+    if (hunger <= 100){
+        hunger +=10;
+        petAffection +=1;}
+        else if (hunger >=100) {
+            hunger -=0;
+            petAffection -=0;
+        }
     console.log('i am become food')
     updateView();   
     
@@ -50,11 +61,43 @@ function wash(){ // Vasking av dyret
 }
 
 function drink(){ // Drikke til dyret
-    thirst += 10;
-    petAffection += 1;
+    //thirst += 10;
+    //petAffection += 1;
     console.log('i am become drink')
+    if (thirst <= 100){
+    thirst +=10;
+    petAffection +=1;}
+    else if (thirst >=100) {
+        thirst -=0;
+        petAffection -=0;
+    }
     updateView();
 }
+
+function Play(){
+    petAffection += 40;
+    alert('me playing is fun pls more play or me is running from slave master')
+    updateView();
+}
+
+function Sleep(){ // for at dyret skal sove
+    thirst -=20
+    hunger -=20
+    updateView();
+}
+
+function buyTamaCoin(){ // microtransaction
+    alert('enter your credit card details')
+}
+
+
+
+
+
+
+
+
+
 
 
 
